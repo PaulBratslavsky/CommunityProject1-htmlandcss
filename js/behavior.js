349f2d89f11dad7bab55c6de3ffb39623b0f9737
paul bratslavsky <paulbratslavsky@pauls-MacBook-Pro.local>
2021-05-02 10:37:55 -0500

    fixed bug when form woul allow submit when not all fields complete

--- js/behavior.js
+++ js/behavior.js
@@ -2,11 +2,8 @@
   REGEX FOR FORM VALIDATION
 *****************************************************/
 const checkNameRegex = /^[A-Za-z ]+$/;
-
 const checkPhoneRegex = /^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
-
 const checkEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
 const checkMessageRegex = /^[a-zA-Z0-9.,:;"'$&%!?_+=() \/ -]*$/;
 
 const regex = [
@@ -19,9 +16,6 @@ const regex = [
 /*****************************************************
   HELEPR FUNCTIONS
 *****************************************************/
-function trimWhiteSpace(value) {
-  return value.replace(/^\s+/, "").replace(/\s+$/, "");
-}
 
 function removeLastItemsFromArray(items) {
   return items.splice(0, items.length - 1);
@@ -41,17 +35,21 @@ function backToTop(elementSelector) {
 *****************************************************/
 
 function checkInputWithRegex(item, regex) {
-  item.addEventListener("keyup", (e) => {
-    const { name, value } = e.target;
-    const trimmed = trimWhiteSpace(value);
-    result = regex.test(trimmed) && trimmed.length > 0;
-
+  function applyStyling(item, result) {
     item.style.backgroundColor = result ? "#186918" : "#822554";
     item.style.color = "white";
     item.dataset.valid = result ? "true" : "false";
+  }
 
-    console.log(name, value, trimmed, regex, result);
-  });
+  function handleKeyUp(e) {
+    const { name, value } = e.target;
+    const trimmed = value.trim();
+    result = regex.test(trimmed) && trimmed.length > 0;
+    applyStyling(item, result);
+    // console.log(name, value, trimmed, regex, result);
+  }
+
+  item.addEventListener("keyup", handleKeyUp);
 }
 
 function handleFormSubmission(formId, inputValidation, regex) {
@@ -59,11 +57,8 @@ function handleFormSubmission(formId, inputValidation, regex) {
   const elementsArray = Array.from(node);
   const inputItemsOnly = removeLastItemsFromArray(elementsArray);
 
-  inputItemsOnly.forEach((item, index) => inputValidation(item, regex[index]));
-
   function resetForm(items) {
     items.forEach((item) => {
-      console.log(item.dataset.valid);
       item.style.backgroundColor = "#363737";
       item.dataset.valid = "false";
       item.value = "";
@@ -72,31 +67,39 @@ function handleFormSubmission(formId, inputValidation, regex) {
 
   function sendData(items) {
     const dataToSend = {};
-    inputItemsOnly.forEach((item) => (dataToSend[item.name] = item.value));
+    items.forEach((item) => (dataToSend[item.name] = item.value));
     console.log(dataToSend, "sending this data");
+  }
+
+  function findError(items) {
+    return items.filter((item) => item.isValid !== true);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    let isValid = false;
+    let isValid = [];
 
     inputItemsOnly.forEach((item) => {
-      console.log(item.dataset.valid);
       item.style.backgroundColor =
         item.dataset.valid == "true" ? "#186918" : "#822554";
-      isValid = item.dataset.valid == "true";
+        
+      isValid.push({
+        fieldId: item.id,
+        isValid: item.dataset.valid == "true",
+        value: item.value,
+        label: item.dataset.label,
+      });
     });
 
-    if (isValid) {
+    if (!findError(isValid).length) {
       sendData(inputItemsOnly);
       resetForm(inputItemsOnly);
       alert("Great Success!");
-    } else {
-      alert("Complete All Requierd Fields");
     }
   }
 
+  inputItemsOnly.forEach((item, index) => inputValidation(item, regex[index]));
   node.addEventListener("submit", handleSubmit);
 }
 
